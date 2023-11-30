d9b1c652c03eac76a0ef0332b48080d84d27ee54
Jeehyun Yoon (Peter) <dbswlgus0203@gmail.com>
2023-11-30 00:40:38 -0500

    #10 - Spring Data JPA properties and setting
    * I add 'jpaConfi' to use jpa auditing
    * Applying Various Boot Properties for Database Access and Development Convenience
    *TODO: I expose the password. HAVE TO HANDLE

---
+++ project-board/src/main/java/com/meopang/board/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.meopang.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return () -> Optional.of("peter"); // TODO: When I add authorization method, I will fix it
+    }
+}
