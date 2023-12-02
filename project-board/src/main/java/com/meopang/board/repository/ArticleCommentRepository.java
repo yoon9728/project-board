3ddbc3e13509087bb9147de3ebea6d094d4dcfbd
Jeehyun Yoon (Peter) <dbswlgus0203@gmail.com>
2023-12-02 03:34:30 -0500

    #1 - data REST setting
    I  have configured the system to automatically generate RESTful JSON APIs for articles and comments.

--- project-board/src/main/java/com/meopang/board/repository/ArticleCommentRepository.java
+++ project-board/src/main/java/com/meopang/board/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.meopang.board.repository;
 
 import com.meopang.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
